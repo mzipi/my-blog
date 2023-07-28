60d8e8664fb30a9d1e55ecd49b2ede1ec23e0503
Michell Zipitría <mzipi@outlook.com>
2023-07-28 18:07:27 -0300

    probando error page

--- src/app/blog/[id]/comments/error.tsx
+++ src/app/blog/[id]/comments/error.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
-
-interface ErrorProps {
-  error: string;
-  reset: () => void;
-}
-
-const Error: React.FC<ErrorProps> = ({ error, reset }) => {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
+'use client'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
   return (
     <div>
       <h2>Something went wrong!</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
-  );
-};
-
-export default Error;
+  )
+}
