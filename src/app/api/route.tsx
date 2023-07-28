441d6d567952ae389722938c198253010807752f
Michell Zipitría <mzipi@outlook.com>
2023-07-28 18:01:07 -0300

    agregados tipos a api route

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,23 +1,21 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 import { getSortedPostsData, postData } from "./db-connection";
- 
-export async function GET() {
 
-    const res = await getSortedPostsData()
- 
-    return NextResponse.json(res)
+export async function GET(): Promise<NextResponse> {
+    const res = await getSortedPostsData();
+    return NextResponse.json(res);
 }
 
-export async function POST(req) {
-    const res = await req.json()
-    await postData(res)
-    return NextResponse.json({res: 200})
+export async function POST(req: Request): Promise<NextResponse> {
+    const res = await req.json();
+    await postData(res);
+    return NextResponse.json({ res: 200 });
 }
 
-export async function PUT() {
-    return NextResponse.json({res: 200})
+export async function PUT(): Promise<NextResponse> {
+    return NextResponse.json({ res: 200 });
 }
 
-export async function DELETE() {
-    return NextResponse.json({res: 200})
+export async function DELETE(): Promise<NextResponse> {
+    return NextResponse.json({ res: 200 });
 }
