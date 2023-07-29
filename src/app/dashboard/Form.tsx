39ef57e55842ffd94a8804cd6ca0f50c2feba6e3
Michell Zipitría <mzipi@outlook.com>
2023-07-28 22:01:30 -0300

    rows attribute delete from form

--- src/app/dashboard/Form.tsx
+++ src/app/dashboard/Form.tsx
@@ -41,7 +41,7 @@ export function Form() {
                 </div>
                 <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
                     <label htmlFor="editor" className="sr-only">Publish post</label>
-                    <textarea id="editor" rows="8" className="w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required></textarea>
+                    <textarea id="editor" className="w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required></textarea>
                 </div>
             </div>
             <button type="submit" className="px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
