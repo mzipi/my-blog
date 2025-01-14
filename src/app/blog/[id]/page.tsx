ff08cd01415100ee8099a91fed25a103ea14d09e
Michell Zipitría <mzipi@outlook.com>
2025-01-14 21:37:25 +0000

    .

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -3,17 +3,17 @@ import { Header } from "@/app/components/Header"
 import { getPost } from "@/app/lib/db-connection"
 import Comments from "./comments/page"
 
-export default async function Post(params: { id: any }) {
-
+export default async function Post(/*params: { id: string }*/) {
+    /*
     const id = params.id
     const paragraph = await getPost(id)
-
+    */
     return(
         <div>
             <Header></Header>
             <h1 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">{paragraph.title}</h1>
             <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">{paragraph.post}</p>
-            <Comments postId={id}></Comments>
+            <Comments /*postId={id}*/></Comments>
             <Footer></Footer>
         </div>
     )
