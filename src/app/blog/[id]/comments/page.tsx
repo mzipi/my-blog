9c053fdb7c3195d93911f1b8da01689094493e35
Michell Zipitría <mzipi@outlook.com>
2025-01-15 06:28:30 +0000

    .

--- src/app/blog/[id]/comments/page.tsx
+++ src/app/blog/[id]/comments/page.tsx
@@ -1,21 +1,19 @@
-/*
-import { getPost } from "@/app/lib/mongo";
-import SendComment from "./SendComment";
+// import { getPost } from "@/app/lib/mongo";
+// import SendComment from "./SendComment";
 
 export default async function Comments(props: any): Promise<JSX.Element> {
-  const paragraph = await getPost(props.postId);
+  //const paragraph = await getPost(props.postId);
 
   return (
     <div className="w-1/3 rounded-lg bg-gray-700 border-gray-600 px-4 py-2 mt-6">
-      {
+      {/*
         paragraph.comments
         ?
         paragraph.comments.map((element: { id: string; comment: string }) => <small key={element.id}>{element.comment}</small>)
         :
         null
-      }
-      <SendComment></SendComment>
+      */}
+      {/*<SendComment></SendComment>*/}
     </div>
   )
 }
-*/
