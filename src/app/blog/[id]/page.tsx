f16f4505411331708aa651e50c112d4c1ed5e351
Michell Zipitría <mzipi@outlook.com>
2025-01-15 06:31:46 +0000

    .

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,28 +1,28 @@
 import { Footer } from "@/app/components/Footer"
 import { Header } from "@/app/components/Header"
-// import Comments from "./comments/page"
+// import { getPost } from "@/app/lib/db-connection"
+import Comments from "./comments/page"
 
+/*
 interface PostProps {
   params: {
     id: string;
   };
 }
+*/
 
-export default async function BlogPost({ params }: { params: { id: string } }) {
-  const res = await fetch(`/api/posts/${params.id}`, { cache: "no-store" });
-  const post = await res.json();
+export default async function Post(/*{ params }: PostProps*/) {
 
-  if (!post) {
-    return <p>Post not found</p>;
-  }
+    // const { id } = params
+    // const paragraph = await getPost(id)
 
-  return(
-      <div>
-          <Header></Header>
-          <h1 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">{post.title}</h1>
-          <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">{post.post}</p>
-          {/*<Comments postId={post.id}></Comments>*/}
-          <Footer></Footer>
-      </div>
-  )
+    return(
+        <div>
+            <Header></Header>
+            <h1 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">{/*paragraph.title*/}</h1>
+            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">{/*paragraph.post*/}</p>
+            <Comments /*postId={id}*/></Comments>
+            <Footer></Footer>
+        </div>
+    )
 }
