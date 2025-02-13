f30c05e5c4041bd1d956520e3aceef10ddfb06b8
Michell Zipitría <mzipi@outlook.com>
2025-02-13 07:40:19 -0300

    fix vercel

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,13 +1,18 @@
 import { NextResponse } from "next/server";
-import clientPromise from "@/app/lib/mongo";
+import clientPromise from "@/lib/mongo";
 import { ObjectId } from "mongodb";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(req: Request, { params }: { params: Record<string, string> }) {
     try {
         const client = await clientPromise;
         const db = client.db("my-blog");
 
-        const post = await db.collection("entries").findOne({ _id: new ObjectId(params.id) });
+        const { id } = params;
+        if (!ObjectId.isValid(id)) {
+            return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+        }
+
+        const post = await db.collection("entries").findOne({ _id: new ObjectId(id) });
 
         if (!post) {
             return NextResponse.json({ error: "Post no encontrado" }, { status: 404 });
@@ -19,7 +24,6 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
         return NextResponse.json({ error: "Error interno del servidor" }, { status: 500 });
     }
 }
-
 
 /*
 export async function getSortedPostsData(): Promise<any> {
