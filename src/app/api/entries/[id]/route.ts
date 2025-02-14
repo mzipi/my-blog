904f4edc553d474de6c66b3a7f47f280e9266eb7
Michell Zipitría <mzipi@outlook.com>
2025-02-13 23:35:40 -0300

    params in api/entries/id

--- src/app/api/entries/[id]/route.ts
+++ src/app/api/entries/[id]/route.ts
@@ -2,11 +2,7 @@ import { ObjectId } from 'mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '@/app/lib/mongo';
 
-interface Params {
-    id: string;
-}
-
-export async function GET(req: NextRequest, { params }: { params: Params }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
     try {
         const client = await clientPromise;
         const db = client.db("my-blog");
@@ -26,62 +22,3 @@ export async function GET(req: NextRequest, { params }: { params: Params }) {
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
     }
 }
-
-
-
-/*
-export async function getSortedPostsData(): Promise<any> {
-    try {
-        await client.connect();
-        const db = client.db('my-blog');
-        const collection = db.collection('post').find();
-        return collection.toArray();
-    } catch(e) {
-        console.error(e);
-    }
-}
-
-export async function postData(data: {title: string; post: string; tag: string}): Promise<void> {
-    try {
-        await client.connect();
-        const db = client.db('my-blog');
-        const collection = db.collection('post');
-        await collection.insertOne(data);
-    } catch (e) {
-        console.error(e);
-    } 
-}
-
-export async function getPost(id: string) {
-    try {
-        await client.connect();
-        const db = client.db('my-blog');
-        const collection = await db.collection('post').findOne({_id: new ObjectId(id)});
-        return collection;
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-export default getPost;
-*/
-
-// ----------------------------------------------------------------------------
-                
-/*
-export async function POST(req: Request): Promise<NextResponse> {
-    const res = await req.json();
-    await postData(res);
-    return NextResponse.json({ res: 200 });
-}
-
-export async function PUT(): Promise<NextResponse> {
-    // editar post
-    return NextResponse.json({ res: 200 });
-}
-
-export async function DELETE(): Promise<NextResponse> {
-    // eliminar post
-    return NextResponse.json({ res: 200 });
-}
-    */
