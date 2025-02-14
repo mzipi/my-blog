716ad9df99127cf5ec3334e6df188bfa70fa8aaf
Michell Zipitría <mzipi@outlook.com>
2025-02-14 00:04:17 -0300

    fix vercel

--- src/app/api/entries/[id]/route.ts
+++ src/app/api/entries/[id]/route.ts
@@ -2,11 +2,18 @@ import { ObjectId } from 'mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '@/app/lib/mongo';
 
-export async function GET(req: NextRequest, { params, query }: { params: { id: string }; query: {} }) {
+type Props = {
+    params: Promise<{
+        id: string
+    }>
+}
+
+export async function GET(req: NextRequest, props: Props) {
     try {
         const client = await clientPromise;
         const db = client.db("my-blog");
-        
+        const params = await props.params
+
         if (!ObjectId.isValid(params.id)) {
             return NextResponse.json({ error: "Invalid ObjectId format" }, { status: 400 });
         }
