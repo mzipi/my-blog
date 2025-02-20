fd21905bca61f44505d532cb7264bf0e25c41bf8
Michell Zipitría <mzipi@outlook.com>
2025-02-20 05:22:13 -0300

    refactor: update GET handler to use Promise for tag parameter retrieval

--- app/api/entries/tag/[tag]/route.ts
+++ app/api/entries/tag/[tag]/route.ts
@@ -2,10 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import connectToDatabase from "@/lib/mongo";
 import { Entry } from "@/models/entries";
 
-export async function GET(req: NextRequest, { params }: { params: { tag: string } }) {
+type Props = {
+    params: Promise<{
+        tag: string
+    }>
+}
+
+export async function GET(req: NextRequest, props: Props) {
     await connectToDatabase();
 
+    const params = await props.params;
     const posts = await Entry.find({ tags: params.tag });
+    
     if (!posts.length) {
         return NextResponse.json({ error: "No hay posts con esta etiqueta" }, { status: 404 });
     }
